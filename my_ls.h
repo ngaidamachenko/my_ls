c2256e1d838bca90955130bf37e66f2d3ee4d77f
nikita gaidamachenko <gaidamac_n@qwasar.io>
2024-10-14 22:04:31 +0000

    adding .c .h and makefile

---
+++ my_ls.h
@@ -0,0 +1,25 @@
+#ifndef MY_LS_H
+#define MY_LS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
+
+#define MAX_FILENAME 256
+
+typedef struct FileEntry {
+    char *name;
+    time_t mtime;
+    int is_dir;
+} FileEntry;
+
+void list_directory(const char *path, int show_all, int sort_by_time);
+int compare_mtime(const void *a, const void *b);
+int compare_name(const void *a, const void *b);
+
+#endif
